c93d7b07cb4b231e04b49cd8a83a171dba184618
isaac-svg <isaacsakyi009@gmail.com>
2024-05-22 00:14:10 +0000

    done

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Pagination  function.
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page
+    Args:
+    page(int): page number,
+    page_size(int): number of items to returieve
+    """
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
